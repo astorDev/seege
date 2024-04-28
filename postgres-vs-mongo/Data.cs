8c4ef8490365c519df132ab48da2c342e9a3ea35
Egor Tarasov <pavluchenk0@yandex.ru>
2024-04-28 14:03:27 +0400

    small refactoring

--- postgres-vs-mongo/Data.cs
+++ postgres-vs-mongo/Data.cs
@@ -1,6 +1,4 @@
 using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.Driver;
 
 public record Expense(
     string Id,
@@ -35,37 +33,5 @@ public static class TestData
 
     public static string[] RandomUserIds(int count) {
         return Enumerable.Range(0, count).Select(i => random.Next(0, 1000).ToString()).ToArray();
-    }
-}
-
-public static class MongoFactory
-{
-    public static IMongoCollection<Expense> GetCollection() {
-        var mongoClient = new MongoClient("mongodb://localhost:27017");
-        var db = mongoClient.GetDatabase("performance-battle");
-        return db.GetCollection<Expense>("expenses");
-    }
-}
-
-public class Db : DbContext {
-    public DbSet<ExpenseRecord> Expenses { get; set; } = null!;
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=54321;Database=performance-battle;Username=postgres;Password=postgres;IncludeErrorDetail=true");
-    }
-}
-
-public class ExpenseRecord
-{
-    public string Id { get; set; } = null!;
-    public int Amount { get; set; }
-    public JsonDocument Labels { get; set; } = null!;
-
-    public static ExpenseRecord FromExpense(Expense expense) {
-        return new ExpenseRecord {
-            Id = expense.Id,
-            Amount = expense.Amount,
-            Labels = JsonDocument.Parse(JsonSerializer.Serialize(expense.Labels))
-        };
     }
 }
